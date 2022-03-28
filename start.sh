9800dc51ccf8b5c91482f38069cf601607c4c5e7
zhaocheng <zhaocheng990105@gmail.com>
2022-03-28 11:00:28 +0800

    update shell test

--- start.sh
+++ start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# 每次启动获取最新的版本
 LATEST_VERSION=$(curl -sX GET "https://api.github.com/repos/BanqiJane/Bilibili_Danmuji/releases/latest" | awk '/tag_name/{print $4;exit}' FS='[""]')
 
 # 判断旧的版本时候存在
-if [ ! -f "/danmuji/old" ];then
+if [ -f "/danmuji/old" ];then
+    # 存在说明重启过容器
+    # 将重启时的版本写入新的文件
+    echo ${LATEST_VERSION} > new
+else
     # 不存在说明是第一次启动容器
+    echo "You are running docker for the first time"
+    echo "Downloading files now"
     # 将当前版本写入old文件
     echo ${LATEST_VERSION} > old
     # 下载版本
@@ -13,16 +20,19 @@ if [ ! -f "/danmuji/old" ];then
     # 将BiliBili_Danmuji-版本beta.jar 移动至/danmuji文件夹下，并重命名为danmuji.jar
     cp $(find ./ -name "*.jar") danmuji.jar
     # 删除下载的压缩文件
-    rm -rf danmuji danmuji.zip
-else
-    # 存在说明重启过容器
-    # 将重启时的版本写入新的文件
-    echo ${LATEST_VERSION} > new
+    rm -rf danmuji/ danmuji.zip
+    echo "Download completed"
 fi
 
+# 判断新的版本时候存在
 if [ -f "/danmuji/new" ];then
+    # 如果存在
     # 判断版本是否不一样
     if [$(cat old) != $(cat new)];then
+        echo "Inconsistent versions"
+        echo "Version being updated"
+        # 删除旧的danmuji.jar
+        rm danmuji.jar
         # 不一样，获取最新的版本
         # 下载新的版本
         wget https://github.do/https://github.com/BanqiJane/Bilibili_Danmuji/releases/download/$(cat new)/danmuji.zip
@@ -31,9 +41,11 @@ if [ -f "/danmuji/new" ];then
         # 将BiliBili_Danmuji-版本beta.jar 移动至/danmuji文件夹下，并重命名为danmuji.jar
         cp $(find ./ -name "*.jar") danmuji.jar
         # 删除下载的压缩文件
-        rm -rf danmuji danmuji.zip
+        rm -rf danmuji/ danmuji.zip
         # 并将new改名为old
         mv /danmuji/new /danmuji/old
+
+        echo "Update completed"
     fi
 fi
 
